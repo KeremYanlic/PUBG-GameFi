90a38f156b9648c947fc93126a66da49457f7ab5
KeremYanlic <keremyanlc@outlook.com>
2024-01-24 19:27:04 +0300

    Misc file

---
+++ Misc/Enum.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponModel
+{
+    Pistol1911,
+    M16
+}
+public enum AmmoType
+{
+    RifleAmmo,
+    PistolAmmo
+}
+
+public enum ShootingMode
+{
+    Single,
+    Burst,
+    Auto
+}
---
+++ Misc/Settings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Settings
+{
+    public static string targetTag = "Target";
+    public static string wallTag = "Wall";
+    public static string beerBottleTag = "BeerBottle";
+}
